7b04db50d02d50ae0a6b2b9d4fed2398d7543c03
SmailZA <30341797+SmailZA@users.noreply.github.com>
2019-10-29 12:49:33 +0100

    Add files via upload

---
+++ Assets/GraphFunctionName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GraphFunctionName
+{
+    Sine,
+    Sine2D,
+    Sine2D1,
+    MultiSine,
+    MultiSine2D,
+    Ripple,
+    Cylinder,
+    Sphere,
+    Torus
+}
